694e47e99db34fa5e385f146af036c965e84b4f0
Aubin65 <amorlas65@gmail.com>
2024-12-09 15:04:49 +0100

    Ajout de la structure de l'ETL (classes et fonctions)

--- workflows/etl.py
+++ workflows/etl.py
@@ -3,7 +3,44 @@ Ce fichier est utilisé pour mettre en place l'ETL grâce à Apache Luigi
 """
 
 import luigi
+import requests  # noqa
 
 
 class ExtractEarthquake(luigi.Task):
-    pass
+    """Tâche d'extraction de la donnée"""
+
+    def output(self):
+        """Chemin de chargement temporaire de la donnée"""
+        return luigi.LocalTarget("chemin")
+
+    def run(self):
+        pass
+
+
+class TransformEarthquake(luigi.task):
+    """Tâche de tranformation de la donnée"""
+
+    def requires(self):
+        """Définition de l'antécédant"""
+        return ExtractEarthquake()
+
+    def output(self):
+        """Chemin de chargement temporaire de la donnée"""
+        return luigi.LocalTarget("chemin")
+
+    def run(self):
+        pass
+
+
+class LoadEarthquake(luigi.task):
+    """Tâche de chargement de la donnée"""
+
+    def requires(self):
+        """Définition de l'antécédant"""
+        return TransformEarthquake()
+
+    def output(self):
+        return luigi.LocalTarget("chemin")
+
+    def run(self):
+        pass
